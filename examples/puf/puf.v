92181b2591883b706752fec9f291b5dadb227fef
kiwih <hammondpearce@outlook.com>
2025-01-30 08:18:39 +1100

    Change PUF from 127 NOTs to 7 NOTs

--- examples/puf/puf.v
+++ examples/puf/puf.v
@@ -5,12 +5,12 @@ module puf(
     output reg ICE_LED //, RGB_R, RGB_G, RGB_B
 );
 
-//instantiate 128 NOT gates (as raw LUTs)
-wire [127:0] lut_ins, lut_outs;
+//instantiate 7 NOT gates (as raw LUTs)
+wire [6:0] lut_ins, lut_outs;
 (* keep *)
 SB_LUT4 #(
     .LUT_INIT(16'h0001)
-) luts [127:0] (
+) luts [6:0] (
     .I0(lut_ins),
     .I1(1'b0),
     .I2(1'b0),
@@ -19,16 +19,16 @@ SB_LUT4 #(
 );
 
 //wire all the NOT gates in a ring
-assign lut_ins = {lut_outs[126:0], lut_outs[127]};
+assign lut_ins = {lut_outs[5:0], lut_outs[6]};
 
 wire puf_clk;
-assign puf_clk = lut_outs[127];
+assign puf_clk = lut_outs[6];
 
-reg [20:0] counter = 0;
+reg [24:0] counter = 0;
 
 always@(posedge puf_clk) begin
     counter <= counter + 1;
-    ICE_LED <= counter[20];
+    ICE_LED <= counter[24];
 end
 
 endmodule
