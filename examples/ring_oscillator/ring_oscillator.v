6d4a6d46119540c08604b2ecb3febf2ad2a14651
kiwih <hammondpearce@outlook.com>
2025-06-16 13:43:33 +1000

    Improve ring oscillator to be more different between boards by having 70 not gates

--- examples/ring_oscillator/ring_oscillator.v
+++ examples/ring_oscillator/ring_oscillator.v
@@ -5,12 +5,12 @@ module ring_oscillator(
     output reg ICE_LED //, RGB_R, RGB_G, RGB_B
 );
 
-//instantiate 7 NOT gates (as raw LUTs)
-wire [6:0] lut_ins, lut_outs;
+//instantiate 70 NOT gates (as raw LUTs)
+wire [69:0] lut_ins, lut_outs;
 (* keep *)
 SB_LUT4 #(
     .LUT_INIT(16'h0001)
-) luts [6:0] (
+) luts [69:0] (
     .I0(lut_ins),
     .I1(1'b0),
     .I2(1'b0),
@@ -19,16 +19,16 @@ SB_LUT4 #(
 );
 
 //wire all the NOT gates in a ring
-assign lut_ins = {lut_outs[5:0], lut_outs[6]};
+assign lut_ins = {lut_outs[68:0], lut_outs[69]};
 
 wire puf_clk;
-assign puf_clk = lut_outs[6];
+assign puf_clk = lut_outs[69];
 
-reg [24:0] counter = 0;
+reg [20:0] counter = 0;
 
 always@(posedge puf_clk) begin
     counter <= counter + 1;
-    ICE_LED <= counter[24];
+    ICE_LED <= counter[20];
 end
 
 endmodule
