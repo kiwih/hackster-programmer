1838ef39fce90573435bc2ea9fc3c8d1b666c483
kiwih <hammondpearce@outlook.com>
2026-02-23 17:22:19 +1100

    Making new version of power_aes_no_io which is actually 4 cycles, first step was to move the shiftrow before sboxes always

---
+++ examples/power_aes_no_io_4cycles/aes_core_tb.v
@@ -0,0 +1,129 @@
+`default_nettype none
+//set time unit to ns
+`timescale 1ns/1ns
+
+module aes_core_tb();
+
+    reg clk;
+    reg rst_n;
+    reg load_i;
+    reg [127:0] data_i;
+    reg [1:0] size_i = 2'd0;
+    reg dec_i = 0;
+    wire [127:0] data_o;
+    wire busy_o;
+
+    aes_core_static_multicycle_4sbox_128 #(
+        .KEY(128'h00112233445566778899aabbccddeeff)
+    ) dut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .load_i(load_i),
+        .data_i(data_i),
+        .dec_i(dec_i),
+        .data_o(data_o),
+        .busy_o(busy_o)
+    );
+
+    initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars(0, dut);
+        $display("Starting simulation");
+    end
+
+    reg [127:0] plaintext;
+    reg [127:0] ciphertext;
+
+    reg [127:0] data_o_expected;
+
+    initial begin
+        plaintext = 128'h00112233445566778899aabbccddeeff;
+        ciphertext = 128'h62f679be2bf0d931641e039ca3401bb2;
+
+        rst_n = 1;
+        clk = 0;
+        load_i = 1'b0;
+        //128-bit data
+        data_i = plaintext;
+
+        //reset the circuit
+        rst_n = 0;
+        #1; clk = 1; #1; clk = 0;
+        rst_n = 1;
+
+        //run one clock cycle
+        #1; clk = 1; #1; clk = 0;
+
+        //set start signal
+        load_i = 1'b1;
+
+        //run one clock cycle
+        #1; clk = 1; #1; clk = 0;
+
+        //clear start signal
+        load_i = 1'b0;
+
+        //make sure busy signal is high
+        if(busy_o != 1) begin
+            $display("Error: busy signal is not high");
+            $finish;
+        end
+        
+
+        //run until busy signal is low or timeout
+        for(int i = 0; i < 100 && busy_o == 1; i++) begin
+            $display("Busy: %b, Data: %h", busy_o, data_o);
+            #1; clk = 1; #1; clk = 0;
+        end
+
+        if(busy_o == 1) begin
+            $display("Error: timeout waiting for busy signal to go low");
+            $finish;
+        end
+        $display("Busy: %b, Data: %h", busy_o, data_o);
+
+        //check output
+        if(data_o != ciphertext) begin
+            $display("Error: data_o does not match expected value");
+            $finish;
+        end
+
+        $display("Encryption successful");
+
+        data_i = ciphertext;
+        dec_i = 1;
+        load_i = 1;
+
+        //run one clock cycle
+        #1; clk = 1; #1; clk = 0;
+
+        //clear start signal
+        load_i = 1'b0;
+
+        //make sure busy signal is high
+        if(busy_o != 1) begin
+            $display("Error: busy signal is not high");
+            $finish;
+        end
+        
+
+        //run until busy signal is low
+        while(busy_o == 1) begin
+            $display("Busy: %b, Data: %h", busy_o, data_o);
+            #1; clk = 1; #1; clk = 0;
+        end
+
+        $display("Busy: %b, Data: %h", busy_o, data_o);
+
+        //check output
+        if(data_o != plaintext) begin
+            $display("Error: data_o does not match expected value");
+            $finish;
+        end
+
+        $display("Decryption successful");
+
+        $finish;
+    end
+
+endmodule
---
+++ examples/power_aes_no_io_4cycles/top.v
@@ -0,0 +1,84 @@
+`default_nettype none
+
+module top(
+    input wire ICE_CLK,
+    output wire ICE_LED, RGB_R, RGB_G, RGB_B
+);
+
+
+reg resetn = 1'b1;
+reg did_reset = 1'b0;
+always @(posedge ICE_CLK) begin
+    if(did_reset == 1'b0 && resetn == 1'b1) begin
+        resetn <= 1'b0;
+    end else if (resetn == 1'b0) begin
+        did_reset <= 1'b1;
+        resetn <= 1'b1;
+    end
+end
+
+reg aes_start;
+wire aes_busy;
+
+//128 bit LFSR
+reg [127:0] lfsr_reg = 128'hACE1ACE159C359C3B386B386670D670C;
+//put taps at 127, 109, 85, 0
+wire lfsr_out;
+assign lfsr_out = lfsr_reg[127] ^ lfsr_reg[109] ^ lfsr_reg[85] ^ lfsr_reg[0];
+reg lfsr_shift_en;
+always @(posedge ICE_CLK) begin
+    if(resetn == 1'b0) begin
+        lfsr_reg <= 128'hACE1ACE159C359C3B386B386670D670C;
+    end else if(lfsr_shift_en == 1'b1) begin
+        lfsr_reg <= {lfsr_reg[126:0], lfsr_out};
+    end
+end
+
+
+wire [127:0] aes_text_in = lfsr_reg; //the LFSR
+wire [127:0] aes_text_out;
+wire [127:0] aes_r10_key;
+
+
+aes_core_static_multicycle_4sbox_128 #(
+    .KEY(128'h00112233445566778899aabbccddeeff)
+) aes_core (
+    .clk        (ICE_CLK),
+    .rst_n      (resetn),
+    .load_i     (aes_start),
+    .data_i     (aes_text_in),
+    .dec_i      (1'b1),
+    .data_o     (aes_text_out),
+    .busy_o     (aes_busy)
+);
+
+reg [7:0] counter = 0;
+
+always @(posedge ICE_CLK) begin
+    aes_start <= 1'b0;
+    lfsr_shift_en <= 1'b0;
+    //if(aes_busy == 1'b1) begin
+    //    counter <= 0;
+    //end else if(aes_busy == 1'b0) begin
+    counter <= counter + 1;
+    if(counter == 8'hEF) begin 
+        lfsr_shift_en <= 1'b1;
+    end else if(counter == 8'hF0) begin
+        aes_start <= 1'b1;
+    end else if(counter == 8'hFF) begin
+        counter <= 0;
+    end
+    //end
+end
+
+assign ICE_LED = (counter >= 8'hF0);
+
+//take the logical OR of all the output aes_text_out and assign it to RGB_R
+assign RGB_R = aes_text_out[0];
+//take the logical AND of all the output aes_text_out and assign it to RGB_G
+assign RGB_G = aes_text_out[127];
+////take the logical XOR of all the output aes_text_out and assign it to RGB_B
+//assign RGB_B = ^aes_text_out[127:0];0
+assign RGB_B = aes_text_out[63];
+//ra
+endmodule
